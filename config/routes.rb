3797afc912ec2a01c9e8941cf784f5c604c7fc2d
LuzIreneSeedTech <cortez.luzirene@seedtech.ph>
2024-03-14 15:55:12 +0800

    Admin CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  namespace :admin do
+    resources :categories
+  end
+
+  devise_for :admins
+
+  get 'home' => 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +14,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'home#index'
+
+  authenticated :admin_user do
+    root to: 'admin#index', as: :admin_root
+  end
+
+  get 'admin' => 'admin#index'
 end
+
+# 14:00
